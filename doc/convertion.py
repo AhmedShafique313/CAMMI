2461fb5642bd9a6864d10f40bfca58ccfe435968
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2025-09-25 13:30:04 +0500

    Docx to pdf conversion is working on local but with sandbox api key of convertapi

--- doc/convertion.py
+++ doc/convertion.py
@@ -1,32 +1,18 @@
-import requests, json, base64
+import requests, json, base64, os
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=r"C:\Users\Kavtech AI Engineer\Downloads\Projects\CAMMI\.env")
 
 # Your API secret key from ConvertAPI dashboard
-API_KEY = ""
+CONVERTAPI_KEY = os.getenv("CONVERTAPI_KEY")
 
-url = "https://v2.convertapi.com/convert/docx/to/pdf?Secret=" + API_KEY
+url = "https://v2.convertapi.com/convert/docx/to/pdf?Secret=" + CONVERTAPI_KEY
 
 files = {"File": open(r"C:\Users\Kavtech AI Engineer\Downloads\Projects\CAMMI\doc\gtm.docx", "rb")}
 response = requests.post(url, files=files)
 result = response.json()
 
-print(json.dumps(result, indent=2))
-
-if "Files" in result:
-    file_info = result["Files"][0]
-
-    if "Url" in file_info:
-        # Case 1: API returns a hosted URL
-        print("PDF available at:", file_info["Url"])
-
-    elif "FileData" in file_info:
-        # Case 2: API returned the Base64 file
-        pdf_bytes = base64.b64decode(file_info["FileData"])
-        with open("output.pdf", "wb") as f:
-            f.write(pdf_bytes)
-        print("PDF saved locally as output.pdf")
-
-    else:
-        print("Unexpected file response:", file_info)
-
+if "Files" in result and "FileData" in result["Files"][0]:
+    base64_pdf = result["Files"][0]["FileData"]
+    print(base64_pdf)
 else:
-    print("Error:", result)
+    print("Invalid document")
